042f8c917ddb65ca41ecc5dc74f4e00c6a91d64b
Tobias Wink <Tobias.Wink@lindenbaum.eu>
2018-11-17 12:35:32 +0100

    initial commit

---
+++ src/main/java/de/sainth/recipemanager/RecipemanagerApplication.java
@@ -0,0 +1,12 @@
+package de.sainth.recipemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipemanagerApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sainth/recipemanager/RecipemanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sainth.recipemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipemanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
