a6a2ccae462e80f4ee39ae3076eaea1fe52aad6d
Tobias Wink <Tobias.Wink@lindenbaum.eu>
2018-11-18 21:02:49 +0100

    Extract RecipeController

--- src/main/java/de/sainth/recipemanager/RecipeController.java
+++ src/main/java/de/sainth/recipemanager/RecipeController.java
@@ -17,31 +17,46 @@
 
 package de.sainth.recipemanager;
 
-import java.util.List;
 import java.util.Map;
 
 import de.sainth.recipemanager.db.RecipeRepository;
-import de.sainth.recipemanager.db.model.OverviewRecipe;
+import de.sainth.recipemanager.db.model.Recipe;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+@RequestMapping("/recipe")
+public class RecipeController {
   private final RecipeRepository recipeRepository;
 
   @Autowired
-  public HomeController(RecipeRepository recipeRepository) {
+  public RecipeController(RecipeRepository recipeRepository) {
     this.recipeRepository = recipeRepository;
   }
 
-  @GetMapping("/")
-  public ModelAndView index(Map<String, Object> model) {
-    List<OverviewRecipe> recipes = recipeRepository.overviewQuery();
-    model.put("recipes", recipes);
-    return new ModelAndView("index", model);
+  @GetMapping("/show")
+  public ModelAndView showRecipe(@RequestParam long id, Map<String, Object> model) {
+    Recipe recipe = recipeRepository.query(id);
+    model.put("recipe", recipe);
+    return new ModelAndView("showRecipe", model);
   }
 
+  @GetMapping("/create")
+  public String createRecipe() {
+    return "createRecipe";
+  }
+
+  @PostMapping("/create")
+  public String authenticate(@RequestParam Map<String, String> map) {
+    Recipe recipe = new Recipe(map.get("name"), Short.valueOf(map.get("portions")));
+    recipe.setDescription(map.get("description"));
+    recipeRepository.add(recipe);
+    return "redirect:/";
+  }
 }
